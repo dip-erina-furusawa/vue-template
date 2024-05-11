8acc4b5d4568dfa9ae94f15367f0f1bc01d59517
erina-furusawa <erina-furusawa@dip-net.co.jp>
2024-05-11 18:21:43 +0900

    install unplugin-auto-import

--- vite.config.ts
+++ vite.config.ts
@@ -1,5 +1,5 @@
 import { fileURLToPath, URL } from 'node:url'
-
+import AutoImport from 'unplugin-auto-import/vite'
 import { defineConfig } from 'vite'
 import vue from '@vitejs/plugin-vue'
 import vueJsx from '@vitejs/plugin-vue-jsx'
@@ -11,6 +11,66 @@ export default defineConfig({
     vue(),
     vueJsx(),
     VueDevTools(),
+    AutoImport({
+      // targets to transform
+      include: [
+        /\.[tj]sx?$/, // .ts, .tsx, .js, .jsx
+        /\.vue$/,
+        /\.vue\?vue/, // .vue
+        /\.md$/ // .md
+      ],
+
+      // global imports to register
+      imports: [
+        // presets
+        'vue',
+        // custom
+        {
+          axios: [
+            // default imports
+            ['default', 'axios'] // import { default as axios } from 'axios',
+          ]
+        }
+      ],
+
+      // Enable auto import by filename for default module exports under directories
+      defaultExportByFilename: false,
+
+      // Auto import for module exports under directories
+      // by default it only scan one level of modules under the directory
+      dirs: ['./src/components'],
+      // Filepath to generate corresponding .d.ts file.
+      // Defaults to './auto-imports.d.ts' when `typescript` is installed locally.
+      // Set `false` to disable.
+      dts: './auto-imports.d.ts',
+
+      // Array of strings of regexes that contains imports meant to be ignored during
+      // the declaration file generation. You may find this useful when you need to provide
+      // a custom signature for a function.
+      ignoreDts: ['ignoredFunction', /^ignore_/],
+
+      // Auto import inside Vue template
+      // see https://github.com/unjs/unimport/pull/15 and https://github.com/unjs/unimport/pull/72
+      vueTemplate: false,
+
+      // Custom resolvers, compatible with `unplugin-vue-components`
+      // see https://github.com/antfu/unplugin-auto-import/pull/23/
+      resolvers: [
+        /* ... */
+      ],
+
+      // Inject the imports at the end of other imports
+      injectAtEnd: true,
+
+      // Generate corresponding .eslintrc-auto-import.json file.
+      // eslint globals Docs - https://eslint.org/docs/user-guide/configuring/language-options#specifying-globals
+      eslintrc: {
+        enabled: true,
+        // provide path ending with `.mjs` or `.cjs` to generate the file with the respective format
+        filepath: './.eslintrc-auto-import.json', // Default `./.eslintrc-auto-import.json`
+        globalsPropValue: true // Default `true`, (true | false | 'readonly' | 'readable' | 'writable' | 'writeable')
+      }
+    })
   ],
   resolve: {
     alias: {
