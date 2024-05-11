5e863b485ee2a20b322dc8c0d7e6a8a6e77b749d
erina-furusawa <erina-furusawa@dip-net.co.jp>
2024-05-11 18:37:36 +0900

    install unplugin-vue-components

--- vite.config.ts
+++ vite.config.ts
@@ -1,5 +1,6 @@
 import { fileURLToPath, URL } from 'node:url'
 import AutoImport from 'unplugin-auto-import/vite'
+import Components from 'unplugin-vue-components/vite'
 import { defineConfig } from 'vite'
 import vue from '@vitejs/plugin-vue'
 import vueJsx from '@vitejs/plugin-vue-jsx'
@@ -70,6 +71,63 @@ export default defineConfig({
         filepath: './.eslintrc-auto-import.json', // Default `./.eslintrc-auto-import.json`
         globalsPropValue: true // Default `true`, (true | false | 'readonly' | 'readable' | 'writable' | 'writeable')
       }
+    }),
+    Components({
+      // relative paths to the directory to search for components.
+      dirs: ['src/components'],
+
+      // valid file extensions for components.
+      extensions: ['vue'],
+
+      // Glob patterns to match file names to be detected as components.
+      // When specified, the `dirs` and `extensions` options will be ignored.
+      globs: ['src/components/*.{vue}'],
+
+      // search for subdirectories
+      deep: true,
+
+      // resolvers for custom components
+      resolvers: [],
+
+      // generate `components.d.ts` global declarations,
+      // also accepts a path for custom filename
+      // default: `true` if package typescript is installed
+      dts: true,
+
+      // Allow subdirectories as namespace prefix for components.
+      directoryAsNamespace: false,
+
+      // Collapse same prefixes (camel-sensitive) of folders and components
+      // to prevent duplication inside namespaced component name.
+      // works when `directoryAsNamespace: true`
+      collapseSamePrefixes: false,
+
+      // Subdirectory paths for ignoring namespace prefixes.
+      // works when `directoryAsNamespace: true`
+      globalNamespaces: [],
+
+      // auto import for directives
+      // default: `true` for Vue 3, `false` for Vue 2
+      // Babel is needed to do the transformation for Vue 2, it's disabled by default for performance concerns.
+      // To install Babel, run: `npm install -D @babel/parser`
+      directives: true,
+
+      // Transform path before resolving
+      importPathTransform: (v) => v,
+
+      // Allow for components to override other components with the same name
+      allowOverrides: false,
+
+      // filters for transforming targets
+      include: [/\.vue$/, /\.vue\?vue/],
+      exclude: [/[\\/]node_modules[\\/]/, /[\\/]\.git[\\/]/, /[\\/]\.nuxt[\\/]/],
+
+      // Vue version of project. It will detect automatically if not specified.
+      // Acceptable value: 2 | 2.7 | 3
+      version: 2.7,
+
+      // Only provide types of components in library (registered globally)
+      types: []
     })
   ],
   resolve: {
