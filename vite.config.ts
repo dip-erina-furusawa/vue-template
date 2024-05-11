31d589d4d72d147f251bafef109eb6c69de2454d
erina-furusawa <erina-furusawa@dip-net.co.jp>
2024-05-12 00:12:56 +0900

    update auto import settings

--- vite.config.ts
+++ vite.config.ts
@@ -13,67 +13,11 @@ export default defineConfig({
     vueJsx(),
     VueDevTools(),
     Components({
-      dts: true
+      dts: true,
+      dirs: ['src/components']
     }),
     AutoImport({
-      // targets to transform
-      include: [
-        /\.[tj]sx?$/, // .ts, .tsx, .js, .jsx
-        /\.vue$/,
-        /\.vue\?vue/, // .vue
-        /\.md$/ // .md
-      ],
-
-      // global imports to register
-      imports: [
-        // presets
-        'vue',
-        // custom
-        {
-          axios: [
-            // default imports
-            ['default', 'axios'] // import { default as axios } from 'axios',
-          ]
-        }
-      ],
-
-      // Enable auto import by filename for default module exports under directories
-      defaultExportByFilename: false,
-
-      // Auto import for module exports under directories
-      // by default it only scan one level of modules under the directory
-      dirs: ['./src/components'],
-      // Filepath to generate corresponding .d.ts file.
-      // Defaults to './auto-imports.d.ts' when `typescript` is installed locally.
-      // Set `false` to disable.
-      dts: './auto-imports.d.ts',
-
-      // Array of strings of regexes that contains imports meant to be ignored during
-      // the declaration file generation. You may find this useful when you need to provide
-      // a custom signature for a function.
-      ignoreDts: ['ignoredFunction', /^ignore_/],
-
-      // Auto import inside Vue template
-      // see https://github.com/unjs/unimport/pull/15 and https://github.com/unjs/unimport/pull/72
-      vueTemplate: false,
-
-      // Custom resolvers, compatible with `unplugin-vue-components`
-      // see https://github.com/antfu/unplugin-auto-import/pull/23/
-      resolvers: [
-        /* ... */
-      ],
-
-      // Inject the imports at the end of other imports
-      injectAtEnd: true,
-
-      // Generate corresponding .eslintrc-auto-import.json file.
-      // eslint globals Docs - https://eslint.org/docs/user-guide/configuring/language-options#specifying-globals
-      eslintrc: {
-        enabled: true,
-        // provide path ending with `.mjs` or `.cjs` to generate the file with the respective format
-        filepath: './.eslintrc-auto-import.json', // Default `./.eslintrc-auto-import.json`
-        globalsPropValue: true // Default `true`, (true | false | 'readonly' | 'readable' | 'writable' | 'writeable')
-      }
+      imports: ['vue']
     })
   ],
   resolve: {
